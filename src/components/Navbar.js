1f3acd3fff041581ef7384ef891357eb13ad6ee8
Nate Traugh <n.traugh@yahoo.com>
2022-07-25 13:09:24 -0400

    created navbar and hamburger menu with if else

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,21 @@
 import "./NavbarStyle.css"
-
-import React from 'react'
+import { FaBars, FaTimes } from "react-icons/fa"
+import React, { useState } from 'react'
 import { Link } from "react-router-dom"
 
+
+
 const Navbar = () => {
+
+  const [click, setClick] = useState(false)
+  const handleClick = () => setClick(!click)
   return (
     <div className="header">
         <a href="https://www.linkedin.com/in/nate-traugh-7bb18a20a/">
             <h1>LinkedIn</h1>
         </a>
-        <ul className="nav-menu">
+        {/* if else for hamburger menu */}
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -23,6 +29,10 @@ const Navbar = () => {
             <Link to="/Contact">Contact</Link>
           </li>
         </ul>
+        <div className="hamburger" onClick={handleClick}>
+          {click ? (<FaTimes size={20} style={{color: "white"}} />) 
+            : (<FaBars size={20} style={{color: "white"}} />)}
+        </div>
     </div>
   )
 }
