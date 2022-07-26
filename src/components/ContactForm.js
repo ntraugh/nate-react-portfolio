a9b76e4ed72d04eb3f54cb7b22d1918cfa81198a
Nate Traugh <n.traugh@yahoo.com>
2022-07-25 20:03:14 -0400

    added emailjs to the contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import "./ContactFormStyle.css"
+import emailjs from '@emailjs/browser';
+
 
 const ContactForm = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_h757pq6', 'template_zivpeap', e.target, 'VlQlNjYp5bfyIYVaX')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
   return (
     <div className='form'>
-       <form>
+       <form netlify onSubmit={sendEmail}>
         <label>Name</label>
-        <input typeof='text' placeholder='Name'></input>
+        <input typeof='text' id='name' name='name' placeholder='Name'></input>
         <label>Email</label>
-        <input typeof='email' placeholder='Email'></input>
+        <input typeof='email' id='email' name='email' placeholder='Email'></input>
         <label>Message</label>
-        <textarea rows="6" placeholder='Type message here'></textarea>
-        <button className='btn'>Submit</button>
+        <textarea rows="6" id='message' name='message' placeholder='Type message here'></textarea>
+        <button className='btn' type='sumbit'>Submit</button>
        </form>
     </div>
   )
