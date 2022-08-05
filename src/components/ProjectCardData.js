a92e7b15c537d29f4ecea6f0ca89486d22be91c0
Nate Traugh <n.traugh@yahoo.com>
2022-08-05 13:59:59 -0400

    updated projects

--- src/components/ProjectCardData.js
+++ src/components/ProjectCardData.js
@@ -1,14 +1,22 @@
-import project1 from "../images/trailblaze.PNG"
-import project2 from "../images/map.PNG"
-import project3 from "../images/newweather.PNG"
+import project3 from "../images/trailblaze.PNG"
+import project2 from "../images/FlowerShopHome.png"
+import project1 from "../images/newweather.PNG"
 
 
 export const Projects = [
     {
+      title: "Boutique Now",
+      text:
+        "An E-Commerce store for all your boutique needs. Users must login in order to checkout.",
+      image: project2,
+      website: "https://bouquetnow.herokuapp.com/",
+      repo: "https://github.com/ntraugh/flower-shop"
+    },
+    {
         title: "Maps || The Fingerprint of the Past",
         text:
           "Maps are scattered all over the internet. With this app you can compare and contrast centruries worth of data.  Using OpenWeather API, Leaflet.js, and LOC API allows you to search for any city in the world.",
-        image: project2,
+        image: project3,
         website: "https://hreichgelt.github.io/Maps---The-Fingerprint-of-the-Past/",
         repo: "https://github.com/Hreichgelt/Maps---The-Fingerprint-of-the-Past"
       },
@@ -19,14 +27,7 @@ export const Projects = [
         image: project1,
         website: "https://intense-fjord-45584.herokuapp.com/",
         repo: "https://github.com/mycancel/trailblazers"
-      },
-      {
-        title: "Current Weather",
-        text:
-          "Search for any city in the world and not only be presented with the current weather, but also a background image corresponding to the search. This app mainly uses OpenWeather API for weather and Unsplash for background images. ",
-        image: project3,
-        website: "https://ntraugh.github.io/Weather-App/",
-        repo: "https://github.com/ntraugh/Weather-App"
-      },
+      }
+    
 ]
 
