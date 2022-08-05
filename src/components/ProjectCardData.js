32eab4c85cfef730e3c918abfa40bad060a8b8d7
Nate Traugh <n.traugh@yahoo.com>
2022-08-05 14:18:02 -0400

    fixed project data

--- src/components/ProjectCardData.js
+++ src/components/ProjectCardData.js
@@ -13,20 +13,20 @@ export const Projects = [
       repo: "https://github.com/ntraugh/flower-shop"
     },
     {
-        title: "Maps || The Fingerprint of the Past",
-        text:
-          "Maps are scattered all over the internet. With this app you can compare and contrast centruries worth of data.  Using OpenWeather API, Leaflet.js, and LOC API allows you to search for any city in the world.",
-        image: project3,
-        website: "https://hreichgelt.github.io/Maps---The-Fingerprint-of-the-Past/",
-        repo: "https://github.com/Hreichgelt/Maps---The-Fingerprint-of-the-Past"
-      },
-      {
         title: "Trailblazers",
         text:
           "Using Sequelize, MYSQL, handlebars.js, OAUTH, this app allows the user to search for a state and an activity and they will get parks in return. Users must login to see the favorite feature.",
-        image: project1,
+        image: project3,
         website: "https://intense-fjord-45584.herokuapp.com/",
         repo: "https://github.com/mycancel/trailblazers"
+      },
+      {
+        title: "Current Weather",
+        text:
+          "Search for any city in the world and not only be presented with the current weather for that city, but also a background image that corresponds as well.",
+        image: project1,
+        website: "https://ntraugh.github.io/Weather-App/",
+        repo: "https://github.com/ntraugh/Weather-App"
       }
     
 ]
